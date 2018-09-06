90059cda86192cdc76a56886f95e04e95916bd9d
mikmk1 <43032560+mikmk1@users.noreply.github.com>
2018-09-06 18:03:59 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'X/NZbv/rb4HwlNI0G6Uv/gmerGoIUlp50cQAgR/q10FJc2MorLciKY1uzn5v7injedJrYUGG+xi1y3NOnKfWbmqfnMd6gtre/Bezp7vaXYGXt7ykXWTLG8mtJbRdMlFF/4szS/YYqtLodnD4w+x1nwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
